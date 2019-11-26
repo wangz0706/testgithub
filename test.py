e363634c2a04773e0771388529b10d075884eb14
王真真 <zhenzhen.wang@dianrong.com>
2019-11-26 17:01:31 +0800

    modify test.py

--- test.py
+++ test.py
@@ -5,8 +5,6 @@ print('master-master')
 print('dev分支.')
 print('命令试试master')
 print('clone后push master test')
-<<<<<<< HEAD
 print('m-master')
-=======
 print('git update master')
->>>>>>> 287dcdea72151d16d6e0306aa0a13967a397e78c
+print('ssss')
